607c47fdead9016bb85f1d7d56ce1c7c28d8410f
DanilSilever <Dan.bah2@yandex.ru>
2020-07-15 15:57:04 +0300

    It just works

--- Bah/Assets/Scripts/Teleporter.cs
+++ Bah/Assets/Scripts/Teleporter.cs
@@ -1,12 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class Teleporter : MonoBehaviour
 {
     public Transform player;
     public Transform reciever;
-    
 
     private bool playerIsOverlapping = false;
 
@@ -15,41 +13,40 @@ public class Teleporter : MonoBehaviour
         if (playerIsOverlapping)
         {
             Vector3 portalToPlayer = player.position - transform.position;
-            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
-
+            
             // If this is true: The player has moved across the portal
-            if (dotProduct < 0f)
+            if (portalToPlayer.y < 0f)
             {
-                Vector3 y =  new Vector3 (0f,(player.transform.position.y - transform.position.y),0f);
-                float yDiff = y.y;
-                Debug.Log(yDiff+ ":y");
                 // Teleport him!
-                float rotationDiff = -Quaternion.Angle(transform.rotation, reciever.rotation);
-                rotationDiff += 180;
-                player.Rotate(Vector3.up, rotationDiff);
-
-                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
-                player.position = reciever.position + positionOffset - y;
+                Vector3 upperPos = new Vector3(reciever.position.x, reciever.position.y + 2, reciever.position.z);
+                player.position = upperPos ;
                 
-
                 playerIsOverlapping = false;
-
                 
+            }
+            else
+            {
+                Vector3 lowerPos = new Vector3(reciever.position.x, reciever.position.y - 2, reciever.position.z);
+                player.position = lowerPos ;
                 
+                playerIsOverlapping = false;
             }
         }
     }
 
     void OnTriggerEnter (Collider other)
     {
-        Debug.Log("Залетел");
-        playerIsOverlapping = true;
-        
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = true;
+        }
     }
 
     void OnTriggerExit (Collider other)
     {
-        Debug.Log("ВЫлетел");
-        playerIsOverlapping = false;
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = false;
+        }
     }
 }
